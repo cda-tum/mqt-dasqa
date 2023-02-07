66722f6e139f74c64a7f97ce1a78475f776a76c2
Jagatheesan <jagandecapri@gmail.com>
2023-02-07 13:53:22 +0100

    Add command-line parser to get QASM file path
    Command-line parser is added to obtain QASM file path

--- mqhad/main.py
+++ mqhad/main.py
@@ -1,2 +1,11 @@
-if __name__ == '__main__':
-    pass
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="mqhad cli")
+    parser.add_argument(
+        "--file-path", type=str, help="QASM 2.0 file path", required=True
+    )
+
+    args = parser.parse_args()
+    print("#### Start generating")
+    print("#### Generation ended")
