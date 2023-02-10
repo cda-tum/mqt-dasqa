78675a5d72394b4f150fea62eca5b18aac48ff12
Jagatheesan <jagandecapri@gmail.com>
2023-02-10 23:42:09 +0100

    Enhance utils

--- mqhad/tests/test_utils.py
+++ mqhad/tests/test_utils.py
@@ -6,6 +6,14 @@ class TestUtils:
     def test_check_type_failure(self):
         with pytest.raises(TypeError):
             Utils.check_type("1", int)
+    
+    def test_check_type_failure_with_list(self):
+        with pytest.raises(TypeError):
+            Utils.check_type("1", [int, list])
+    
+    def test_check_type_failure_without_raise_error(self):
+        result = Utils.check_type("1", int, raise_error=False)
+        assert result == False
 
     def test_check_type_success(self):
         result = Utils.check_type(1, int)
--- mqhad/utils/utils.py
+++ mqhad/utils/utils.py
@@ -1,11 +1,23 @@
-from typing import Any
+from typing import Any, Union
+
 
 class Utils:
     @staticmethod
-    def check_type(x, expected_class: Any) -> bool:
-        if not isinstance(x, expected_class):
-            raise TypeError(
-                "Expected object of type {} but got {}".format(expected_class, type(x))
-            )
+    def check_type(
+        x, expected_class: Union[Any, list[Any]], raise_error: bool = True
+    ) -> bool:
+        if isinstance(expected_class, list) == False:
+            expected_class = [expected_class]
+
+        truth = [isinstance(x, expected_class_) for expected_class_ in expected_class]
+        if any(truth) == False:
+            if raise_error == True:
+                raise TypeError(
+                    "Expected object of type {} but got {}".format(
+                        expected_class, type(x)
+                    )
+                )
+            else:
+                return False
         else:
             return True
