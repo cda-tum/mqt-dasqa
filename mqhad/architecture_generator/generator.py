24f8b28d96c415bc0b82ad9e2a2c28ef0ed6c447
Jagatheesan <jagandecapri@gmail.com>
2023-02-10 23:48:11 +0100

    Update generator

--- mqhad/architecture_generator/generator.py
+++ mqhad/architecture_generator/generator.py
@@ -1,13 +1,34 @@
-from collections import OrderedDict
 from mqhad.architecture_generator.generator_base import GeneratorBase
-import rustworkx as rx
+from mqhad.architecture_generator.profile import ProfileBase, Profile
+from mqhad.architecture_generator.layout import LayoutBase, Layout
+from mqhad.utils import Utils
 from qiskit.circuit import QuantumCircuit
-from typing import List, Tuple
 
 
 class Generator(GeneratorBase):
-    def __init__(self, qc: QuantumCircuit = None) -> None:
+    def __init__(
+        self,
+        qc: QuantumCircuit = None,
+        profile: ProfileBase = None,
+        layout: LayoutBase = None,
+    ) -> None:
+        if qc is not None:
+            Utils.check_type(qc, QuantumCircuit)
         self.qc = qc
 
+        if profile is not None:
+            Utils.check_type(profile, ProfileBase)
+        self.profile = profile
+
+        if layout is not None:
+            Utils.check_type(layout, LayoutBase)
+        self.layout = layout
+
     def generate(self):
-        pass
+        if self.profile is None:
+            self.profile = Profile(self.qc)
+        self._ordered_degree, self._adjacency_matrix = self.profile.get_profile()
+
+        if self.layout is None:
+            self.layout = Layout(self._ordered_degree, self._adjacency_matrix)
+        self._dimX, self._dimY, self._qubit_grid = self.layout.get_layout()
--- mqhad/tests/test_generator.py
+++ mqhad/tests/test_generator.py
@@ -1,9 +1,40 @@
 import pytest
 from qiskit import QuantumRegister, QuantumCircuit
 from mqhad.architecture_generator.generator import Generator
-from collections import OrderedDict
+from mqhad.architecture_generator.profile import Profile
+from mqhad.architecture_generator.layout import Layout
 
 
 class TestGenerator:
+    @classmethod
+    def setup_class(cls):
+        """setup any state specific to the execution of the given class (which
+        usually contains tests).
+        """
+        qr = QuantumRegister(5)
+        qc = QuantumCircuit(qr)
+        qc.h([0, 1, 2, 3])
+        qc.x(4)
+        qc.cx(0, 1)
+        qc.h(4)
+        qc.cx(0, 4)
+        qc.h(0)
+        qc.cx(1, 4)
+        qc.cx(2, 4)
+        qc.cx(3, 4)
+        qc.cx(0, 4)
+        cls.qc = qc
+
+    def test_init(self):
+        with pytest.raises(TypeError):
+            Generator(qc=1)
+        with pytest.raises(TypeError):
+            Generator(profile=1)
+        Generator(profile=Profile())
+        with pytest.raises(TypeError):
+            Generator(layout=1)
+        Generator(layout=Layout())
+
     def test_generate(self):
-        pass
+        generator = Generator(qc=self.qc)
+        generator.generate()
