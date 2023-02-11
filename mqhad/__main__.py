7fb3840eaac97023cfff1ad1362ae8c7eceeb265
Jagatheesan <jagandecapri@gmail.com>
2023-02-11 15:47:06 +0100

    Fix stdout message

--- mqhad/__main__.py
+++ mqhad/__main__.py
@@ -6,15 +6,15 @@ from qiskit import QuantumCircuit
 
 
 def flow(args):
-    print("#### Start generating architecture ###")
+    print("#### Start generating architecture ####")
     absolute_path = os.path.abspath(args.file_path)
     qc = QuantumCircuit.from_qasm_file(absolute_path)
     generator = Generator(qc=qc)
     qubit_grid, qubit_frequencies = generator.generate()
-    print("#### Architecture generated ###")
+    print("#### Architecture generated ####")
     print("Qubit grid:", qubit_grid)
     print("Qubit frequencies:", qubit_frequencies)
-    print("#### Generation ended ###")
+    print("#### Generation ended ####")
 
 
 def main(args=None):
