8ba18574895c5109279ec7600d281c9a774eb42b
Jagatheesan <jagandecapri@gmail.com>
2023-02-12 01:00:10 +0100

    Commit test for Launchpad

--- mqhad/tests/designer/test_metal_launchpad.py
+++ mqhad/tests/designer/test_metal_launchpad.py
@@ -4,3 +4,6 @@ from mqhad.designer.launchpad.metal import Launchpad
 class TestMetalLaunchpad:
     def test_generate_launchpad(self):
         pass
+
+    def test_get_configuration(self):
+        pass
