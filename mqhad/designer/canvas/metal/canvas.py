2c54f64d910e4b3cbc5e6840b6c5478c1f83f45b
Jagatheesan <jagandecapri@gmail.com>
2023-04-04 12:27:25 +0200

    Move canvas creation to init

--- mqhad/designer/canvas/metal/canvas.py
+++ mqhad/designer/canvas/metal/canvas.py
@@ -4,8 +4,7 @@ from qiskit_metal import designs
 
 class Canvas(CanvasBase):
     def __init__(self):
-        pass
+        self._canvas = designs.DesignPlanar()
 
     def get_canvas(self):
-        self._canvas = designs.DesignPlanar()
         return self._canvas
