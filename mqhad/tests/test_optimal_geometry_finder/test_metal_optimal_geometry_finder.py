1e3bce8ebe03437f195dc93a30bb84b83f8c7d09
Jagatheesan <jagandecapri@gmail.com>
2023-04-09 15:42:24 +0200

    Add test case to optimal geometry finder

--- mqhad/tests/test_optimal_geometry_finder/test_metal_optimal_geometry_finder.py
+++ mqhad/tests/test_optimal_geometry_finder/test_metal_optimal_geometry_finder.py
@@ -1,11 +1,11 @@
-from unittest import mock
+from unittest.mock import patch, MagicMock
 from sklearn.pipeline import Pipeline
 import os
 
 
 class TestOptimalGeometryFinder:
-    @mock.patch("builtins.open")
-    @mock.patch("pickle.load")
+    @patch("builtins.open")
+    @patch("pickle.load")
     def test_unpack_model(self, mock_load, mock_open):
         mock_load.return_value = "model_unpacked"
 
@@ -19,9 +19,10 @@ class TestOptimalGeometryFinder:
             "resonator": None,
         }
 
+        from mqhad.optimal_geometry_finder.optimal_geometry_finder import (
+            OptimalGeometryFinder,
+        )
 
-        from mqhad.optimal_geometry_finder.optimal_geometry_finder import OptimalGeometryFinder
-        
         class OptimalGeometryFinderMock(OptimalGeometryFinder):
             def __init__(self):
                 pass
@@ -53,8 +54,10 @@ class TestOptimalGeometryFinder:
             "resonator": None,
         }
 
-        from mqhad.optimal_geometry_finder.optimal_geometry_finder import OptimalGeometryFinder
-        
+        from mqhad.optimal_geometry_finder.optimal_geometry_finder import (
+            OptimalGeometryFinder,
+        )
+
         class OptimalGeometryFinderMock(OptimalGeometryFinder):
             def __init__(self):
                 pass
@@ -63,23 +66,21 @@ class TestOptimalGeometryFinder:
         optimal_geometry_finder._models = models
         unpacked_models = optimal_geometry_finder._unpack_models()
         assert type(unpacked_models["qubit"]["fQ"]["pad_gap"]) == Pipeline
-    
+
     def test_find_optimal_geometry(self):
-        from mqhad.optimal_geometry_finder.optimal_geometry_finder import OptimalGeometryFinder
-        
+        from mqhad.optimal_geometry_finder.optimal_geometry_finder import (
+            OptimalGeometryFinder,
+        )
+
         class OptimalGeometryFinderMock(OptimalGeometryFinder):
             def __init__(self):
                 pass
 
         optimal_geometry_finder = OptimalGeometryFinderMock()
-        optimal_geometry_finder._models = {
-            "qubit": {
-                "fQ": {
-                    "pad_gap": "model_unpacked",
-                    "pad_height": "model_unpacked",
-                }
-            },
-            "resonator": None,
-        }
-        optimal_geometry = optimal_geometry_finder.find_optimal_geometry("qubit", "fQ", "pad_gap", 0.1)
-        assert optimal_geometry == 0.1
+        mock_metal_optimal_geometry_finder = MagicMock()
+        mock_metal_optimal_geometry_finder.find_optimal_geometry.return_value = "100um"
+        optimal_geometry_finder._optimal_geometry_finder = mock_metal_optimal_geometry_finder
+        optimal_geometry = optimal_geometry_finder.find_optimal_geometry(
+            "qubit", "fQ", 0.1
+        )
+        assert optimal_geometry == "100um"
